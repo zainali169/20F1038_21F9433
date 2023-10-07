f0d447d77aa606542e39e4259d0a011438ee81cc
[Muhammad Ali] <[muhammadali.227k3@gmail.com]>
2023-10-07 19:36:56 +0500

    calculator

---
+++ Calculator/calculator.java
@@ -0,0 +1,19 @@
+package Calculator;
+
+public class calculator {
+
+	int num1=4,num2=2;
+	int result;
+    public int add()
+    {
+    	return result=num1+num2;
+    }
+    public int sub()
+    {
+    	return result=num1-num2;
+    }
+	public calculator() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
